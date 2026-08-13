ca3273bf5dd2b7b6018ac45299b9befa97c5b748
jim-junior <jimjunior854@gmail.com>
2026-08-13 23:17:57 +0300

    fix: syntax error, internal/cli/run.go:93:6
    Signed-off-by: jim-junior <jimjunior854@gmail.com>

--- internal/cli/run.go
+++ internal/cli/run.go
@@ -16,8 +16,8 @@ import (
 	"github.com/urunc-dev/evaluation_suite/internal/orchestrator"
 	"github.com/urunc-dev/evaluation_suite/internal/plan"
 	harnessruntime "github.com/urunc-dev/evaluation_suite/internal/runtime"
-	runtimeHTTPReadiness "github.com/urunc-dev/evaluation_suite/internal/runtime/httpreadiness"
 	runtimeCPU "github.com/urunc-dev/evaluation_suite/internal/runtime/cpu"
+	runtimeHTTPReadiness "github.com/urunc-dev/evaluation_suite/internal/runtime/httpreadiness"
 	runtimeLifecycle "github.com/urunc-dev/evaluation_suite/internal/runtime/lifecycle"
 	runtimeNetwork "github.com/urunc-dev/evaluation_suite/internal/runtime/network"
 	runtimeStorage "github.com/urunc-dev/evaluation_suite/internal/runtime/storage"
@@ -90,7 +90,7 @@ func NewRunCommand() *cobra.Command {
 				},
 				func(trial plan.Trial) (harnessruntime.Adapter, error) {
 					return runtimeCPU.NewAdapter(), nil
-				}
+				},
 			)
 
 			orch := orchestrator.New(adapterFactories...)
